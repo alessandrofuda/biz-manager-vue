6d1a1b3126fd2438fd18240ed98f90f3574e6424
alessandrofuda <alessandro.fuda@gmail.com>
2020-02-13 18:41:57 +0100

    assets init & IT localization Init

--- resources/assets/js/views/layouts/LayoutWizard.vue
+++ resources/assets/js/views/layouts/LayoutWizard.vue
@@ -7,21 +7,21 @@
 </template>
 
 <script type="text/babel">
-export default {
-  watch: {
-    $route: 'onRouteChange'
-  },
-  mounted () {
-    this.setLayoutBackground()
-  },
+    export default {
+      watch: {
+        $route: 'onRouteChange'
+      },
+      mounted () {
+        this.setLayoutBackground()
+      },
 
-  destroyed () {
-    document.body.style.backgroundColor = '#EBF1FA'
-  },
-  methods: {
-    setLayoutBackground () {
-      document.body.style.backgroundColor = '#f9fbff'
+      destroyed () {
+        document.body.style.backgroundColor = '#EBF1FA'
+      },
+      methods: {
+        setLayoutBackground () {
+          document.body.style.backgroundColor = '#f9fbff'
+        }
+      }
     }
-  }
-}
 </script>
